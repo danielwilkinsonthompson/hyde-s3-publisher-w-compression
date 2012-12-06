1ed6aeec4fb741d3f3e3edbb446c88bdbd913b4a
Daniel Wilkinson-Thompson <daniel@wilkinson-thompson.com>
2012-12-06 23:45:00 +1100

    Added MIT license
    Signed-off-by: Daniel Wilkinson-Thompson <daniel@wilkinson-thompson.com>

--- aws.py
+++ aws.py
@@ -1,46 +1,50 @@
 """
-  Sync site to S3
-  ================
+ Sync Hyde Site to S3
+======================
+
+A [Hyde](http://pydoc.net/hyde/latest/) publisher that scans all the files in your site and uploads them to S3 with the same directory structure.
   
-  A Hyde publisher that scans all files in your site and
-  uploads them to S3 with the same directory structure.
+In addition, the script will optionally:
+
+- gzip any HTML, SVG, CSS and Javascript files it finds and add the appropriate 'Content-Encoding' header
+- set an 'Expires' header for each file for optimal caching
+- only upload modified files
+
+Just copy this file to your hyde/ext/publishers/ directory to use it 
+
+Note: This script requires the 'boto' library and a valid entry in your site.yaml file
   
-  In addition, the script will optionally:
-  * gzip compress any HTML, SVG, CSS and Javascript files it 
-    finds and add the appropriate 'Content-Encoding' header.
-  * set a far future 'Expires' header for optimal caching.
-  * only upload files if they have been modified since the 
-    last upload.
-  
-  Copy this file to your hyde/ext/publishers/ directory
-  to use it to upload your site
-  
-  Note: This script requires the Python boto library and a
-  valid entry in the site.yaml file of your Hyde site
+Example site.yaml entry
+-------------------------
+
+      publisher:           
+		  bff: #(call it whatever you want)
+			  type: hyde.ext.publishers.aws.AWS
+			  url: s3://www.yourdomain.com
+			  AWS_ACCESS_KEY_ID: YOURACCESSKEY
+			  AWS_SECRET_ACCESS_KEY: YourSecretSpecialKeyGoesHere     
+              gzip: true (optional) 
+              expires: true (optional)
+			  check_mtime: true (optional)      
+			  verbose: true (optional)
+			  
+Example Usage (Command Line) 
+-----------------------------
+
+	  hyde publish -p bff
     
-  
-  Example site.yaml entry
-  ------------------------
-  
-  publisher:           
-      bff: (call it whatever you want)
-          type: hyde.ext.publishers.aws.AWS
-          url: s3://www.yourdomain.com
-          AWS_ACCESS_KEY_ID: YOURACCESSKEY
-          AWS_SECRET_ACCESS_KEY: YourSecretSpecialKeyGoesHere     
-          expires: true (optional)
-          check_mtime: true (optional)
-          gzip: true (optional)       
-          verbose: true (optional)
-          
-  Example usage (command line)         
-  ----------------------------
-  hyde publish -p bff
-  
-  
-  This code was originally found at:
-   https://texample.googlecode.com/svn-history/r159/trunk/apps/s3sync/management/commands/s3sync.py
-  and has been modified to work with Hyde
+License 
+------------
+
+This is a modification of the Django S3 loader I found [here](https://texample.googlecode.com/svn-history/r159/trunk/apps/s3sync/management/commands/s3sync.py), which was distributed under an MIT license.
+
+Copyright (C) 2012 Daniel Wilkinson-Thompson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
 import datetime
